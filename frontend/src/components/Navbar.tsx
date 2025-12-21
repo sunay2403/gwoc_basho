651ecd5aeacb1e49a95c51203693f34c3e5fe67c
Sunay Bhattacharjee <sunay2403@gmail.com>
2025-12-21 23:34:12 +0530

    navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,58 +4,37 @@ import { NavLink } from "react-router-dom";
 const Navbar: React.FC = () => {
   return (
     <nav className="
-      bg-white
-      sticky
-      top-0
-      z-50
-      h-24
-      flex
-      items-center
-      px-8
-      text-[#C85428]
-    ">
-      <NavLink to="/home" className="flex items-center">
-        <img src={logo} alt="logo" className="h-24" />
-      </NavLink>
+  bg-white
+  sticky top-0 z-50
+  h-20
+  flex items-center
+  px-12
+  text-[#C85428]
+">
+  <NavLink to="/home" className="flex items-center">
+    <img src={logo} className="h-16" />
+  </NavLink>
 
-      <div className="ml-auto flex gap-16 text-xl font-[Montserrat] font-medium">
-        <NavLink
-          to="/home"
-          className={({ isActive }) =>
-            isActive ? "underline underline-offset-8" : ""
-          }
-        >
-          Home
-        </NavLink>
+  <div className="
+    ml-auto
+    flex
+    gap-10
+    text-sm
+    font-medium
+    tracking-wide
+    uppercase
+    whitespace-nowrap
+  ">
+    <NavLink to="/home">Home</NavLink>
+    <NavLink to="/products">Products</NavLink>
+    <NavLink to="/workshops">Workshops</NavLink>
+    <NavLink to="/experiences">Experience</NavLink>
+    <NavLink to="/studio">Studio</NavLink>
+    <NavLink to="/media">Media</NavLink>
+    <NavLink to="/corporate">Corporate</NavLink>
+  </div>
+</nav>
 
-        <NavLink
-          to="/products"
-          className={({ isActive }) =>
-            isActive ? "underline underline-offset-8" : ""
-          }
-        >
-          Products
-        </NavLink>
-
-        <NavLink
-          to="/workshops"
-          className={({ isActive }) =>
-            isActive ? "underline underline-offset-8" : ""
-          }
-        >
-          Workshops
-        </NavLink>
-
-        <NavLink
-          to="/experiences"
-          className={({ isActive }) =>
-            isActive ? "underline underline-offset-8" : ""
-          }
-        >
-          Experience
-        </NavLink>
-      </div>
-    </nav>
   );
 };
 
