7c963476db1ff6a41f413d168346dd8693a3ce3c
Vinay6783 <vahbvinay@gmail.com>
2025-12-27 23:30:57 +0530

    Initial corporate design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import WorkshopsPage from "./pages/Workshops";
 import ExperiencesPage from "./pages/Experiences";
 import BashoMediaSocialProof from "./pages/SocialMedia";
 import StudioPage from "./pages/Studio";
+import CorporatePage from "./pages/Corporate";
 import {
   BrowserRouter,
   Routes,
@@ -31,7 +32,7 @@ const App: React.FC = () => {
           <Route path="/experiences" element={<ExperiencesPage/>} />
           <Route path="/media" element={<BashoMediaSocialProof/>}/>
           <Route path="/studio" element={<StudioPage/>}/>
-
+          <Route path="/corporate" element={<CorporatePage />} />
           {/* Optional: 404 fallback */}
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
