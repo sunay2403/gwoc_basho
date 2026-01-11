18d1e78376c8a6dcd0517087901079da5f22c00c
Sunay Bhattacharjee <sunay2403@gmail.com>
2026-01-11 12:09:43 +0530

    firebase in render

--- backend/config/firebase.py
+++ backend/config/firebase.py
@@ -1,14 +1,15 @@
 # config/firebase.py
 
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-cred = credentials.Certificate(
-    os.path.join(BASE_DIR, "serviceAccountKey.json")
-)
 
 if not firebase_admin._apps:
+    firebase_creds = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+
+    if not firebase_creds:
+        raise Exception("FIREBASE_SERVICE_ACCOUNT environment variable not set")
+
+    cred = credentials.Certificate(json.loads(firebase_creds))
     firebase_admin.initialize_app(cred)
